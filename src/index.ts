6b479dd375e662b674f17555d08f1f76ab09f494
Róbert Kovalčuk <118119299+Robert-Kovalcuk@users.noreply.github.com>
2022-11-22 12:43:07 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 import Observable from "./observer/Observable";
 import Subscriber from "./observer/Subscriber";
 import {watch} from "./utilities/watch";
-import Authentication from "./authentication";
 
-Authentication.currentAuthStatus().then(e => {
-	console.log("asa")
-	console.log(e)
-})
-Authentication.login("test@gmail.com", "test123456").then(r => {
-
-})
